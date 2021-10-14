6ee9faef0b626aa52ef8ec17a79e34ff370cbd61
SSGSSACE <quangbayhai@gmail.com>
2021-10-14 22:13:10 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// alert('Hello World')
+// alert('Tôi là Quang')
+var a=10
+var b=20
+var c=a+b
+//console.log('Hello: ',c)
+setInterval(function() {
+    a=a+1
+    console.log('Hello: ',a)
+},1000)
