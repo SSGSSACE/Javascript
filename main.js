54d6c89e7f891a0c757dbb441135d2ed24fa8bf7
SSGSSACE <quangbayhai@gmail.com>
2021-10-17 00:05:23 +0700

    October 17, 2021 12:05 AM

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var a=10
 var b=20
 var c=a+b
 //console.log('Hello: ',c)
-setInterval(function() {
-    a=a+1
-    console.log('Hello: ',a)
-},1000)
+// setInterval(function() {
+//     a=a+1
+//     console.log('Hello: ',a)
+// },1000)
+var mystring='Hoc Javascript'
+console.log(mystring)
